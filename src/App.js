0a09d784958372c50171110bdfa077980d1473e5
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-10-05 09:02:28 +0300

    getting data from local file

--- src/App.js
+++ src/App.js
@@ -4,14 +4,27 @@ import React from "react";
 import Card from './components/Card';
 import HeroSection from "./components/HeroSection";
 import NavBar from "./components/NavBar";
+import data from "./components/data"
 
 
 function App() {
+  //displaying card information
+  const cards = data.map((item) => {
+    return (
+      <Card 
+        key={item.id}
+        item={item}
+      />
+    )
+  })
+
   return (
     <div>
       <NavBar />
       <HeroSection />
-      <Card />
+      <section className="cards-section">
+        {cards}
+      </section>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,25 @@
 import React from 'react'
 
-function Card() {
+function Card(props) {
+    let displayBadge;
+    if (props.item.stockNumber === 0) {
+        displayBadge= "NOT AVAILABLE"
+    } else {
+        displayBadge= "AVAILABLE"
+    }
 
+    return (
+        <div className='card'>
+            {displayBadge && <div className='card-badge'>{displayBadge}</div>}
+            <img src={props.item.imgSrc} className="card-image"/>
+            <div className='card-statistics'>
+                <p>Rating: {props.item.statistics.rating}</p>
+                <span>({props.item.statistics.reviews} reviews)</span>
+            </div>
+            <p>{props.item.whereTaken}</p>
+            <p className='card-title'>{props.item.title}</p>
+        </div>
+    )
 }
 
 export default Card;
