000286119d37584e777ba82e28beb1f309b2a998
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-11-13 10:54:05 +0300

    additional modifications

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,17 +10,17 @@ function NavBar() {
             <nav>
                 <ul className='bold nav-links'>
                     <li>
-                        <Link to='#favorites' smooth>
+                        <Link to='#favorites' smooth className="links">
                             My Favorites
                         </Link>
                     </li>
                     <li>
-                        <Link to="#photo-collection" smooth>
+                        <Link to="#photo-collection" smooth className="links">
                             All Photo Collections
                         </Link> 
                     </li>
                     <li>
-                        <Link to="#footer" smooth>
+                        <Link to="#footer" smooth className="links">
                             Contact Us
                         </Link>
                     </li>
